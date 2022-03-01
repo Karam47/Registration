c64de0725d598a5c97b31e175fb319fd15f57b96
abrarhannoon <92648528+abrarhannoon@users.noreply.github.com>
2022-03-01 22:21:24 +0200

    update database

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const express=require('express');
 const path = require('path');
 const mysql=require('mysql');
-// const dotenv=require('dotenv');
 
-// dotenv .config({path:'./.env'});
+
 
 const app=express();
 app.set('view engine','hbs');
@@ -14,13 +13,12 @@ let db = mysql.createConnection({
     user:'root',
     password:'',
     database:'sign_up'
-    // port:"3306 "
+   
 })
 const publicDirectory = path.join(__dirname,'./public');
 app.use(express.static(publicDirectory));
 
 app.use(express.urlencoded({extended:false }));
-// app.use(express.json());
 
 db.connect((err)=>{
     if(err){
@@ -29,7 +27,7 @@ db.connect((err)=>{
         console.log('connected');
     }
 })
-// exports.register
+
 app.use('/',require('./routes/pages'));
 app.use('/auth',require('./routes/auth'));
 app.use('/auth2',require('./routes/user'));
